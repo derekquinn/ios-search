70241a48ee3bd8010437d130bb0463d36aa2fc5e
derek quinn <dereq@Troy.local>
2020-11-22 19:52:14 -0500

    Started test cases

---
+++ ios-searchTests/SwapiApiServiceTest.swift
@@ -0,0 +1,76 @@
+//
+//  ios_searchTests.swift
+//  ios-searchTests
+//
+//  Created by derek quinn on 11/22/20.
+//
+
+import XCTest
+@testable import ios_search
+
+class SwapiApiServiceTest: XCTestCase {
+    
+    override func setUp() {
+
+  
+//        do {
+//
+//            let data = try Data(contentsOf: fileUrl, options: .mappedIfSafe)
+//            let mockSuccessfulVehiclesResponse = try JSONDecoder().decode(VehiclesResponse.self, from: data)
+//
+//            print("[TEST] setUp() Results Count = ",mockSuccessfulVehiclesResponse.count)
+//        } catch {
+//
+//        }
+//
+//
+//
+//        let expectedStarshipsResponse: StarshipsResponse
+//        let expectedPeopleResponse: PeopleResponse
+        
+        
+        
+    }
+    
+    func testGetStarshipResults() {
+        
+        let path = Bundle.main.path(forResource: "WeatherResposneSuccess", ofType:"json")
+        
+        print("[HELPER] makeUrlWithFilePath() filepath:", filePath)
+
+        print("[HELPER] makeUrlWithFilePath() fileurl:", fileUrl)
+        XCTAssertTrue(true)
+        
+    }
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+    
+    // Helper functions
+    
+//   func makeUrlWithFilePath(fileName: String) -> URL {
+//        print("[HELPER] makeUrlWithFilePath() filename:", fileName)
+//        let filePath = Bundle.main.path(forResource: fileName, ofType: "json")
+//    print("[HELPER] makeUrlWithFilePath() filepath:", filePath)
+//        let url = URL(fileURLWithPath: filePath!)
+//        return url
+//
+//    }
+
+}
