e3b2172ec396bbedce620b9a4f304a70c56300c7
derek quinn <quinnderek@yahoo.com>
2020-11-23 21:32:39 -0500

    Added button to test values of starships, vehicles & people

--- ios-search/ViewController.swift
+++ ios-search/ViewController.swift
@@ -3,32 +3,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var starshipsResponse: StarshipsResponse?
+    var peopleResponse: PeopleResponse?
+    var vehiclesResponse: VehiclesResponse?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         SwapiApiService.getStarshipResults(parameters: SwapiConstants.paramStarships, completion:{ response in
             
-            let starshipResponse: StarshipsResponse = response
+            self.starshipsResponse = response
             
-            print("[SUCCESS] getStarshipResponse Reached ViewController, RESULTS =",starshipResponse.results?.count ?? 0)
+            print("[SUCCESS] getStarshipResponse Reached ViewController, RESULTS =",self.starshipsResponse!.results!.count )
             
         })
         
         SwapiApiService.getVehicleResults(parameters: SwapiConstants.paramVehicles, completion: { response in
             
-            let vehicleResponse: VehiclesResponse = response
+            self.vehiclesResponse = response
             
-            print("[SUCCESS] getVehicleResponse Reached ViewController, RESULTS =",vehicleResponse.results?.count ?? 0)
+            print("[SUCCESS] getVehicleResponse Reached ViewController, RESULTS =",self.vehiclesResponse!.results!.count)
         })
         
         SwapiApiService.getPeopleResults(paramaters: SwapiConstants.paramPeople, completion:   { response in
             
-            let peopleResponse: PeopleResponse = response
+            self.peopleResponse = response
             
-            print("[SUCCESS] getPeopleResponse Reached ViewController, RESULTS =",peopleResponse.results?.count ?? 0)
+            print("[SUCCESS] getPeopleResponse Reached ViewController, RESULTS =",self.peopleResponse!.results!.count)
         })
         
     }
     
+    @IBAction func logValues() {
+        
+        print("test starships---",self.starshipsResponse?.results?.count ?? 0)
+        print("test vehicles----",self.vehiclesResponse?.results?.count ?? 0)
+        print("test people -----", self.peopleResponse?.results?.count ?? 0)
+    }
+    
 }
-
