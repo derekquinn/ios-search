c4f5542abc8db2ff353d05c9363e45b49ff99ea1
derek quinn <quinnderek@yahoo.com>
2020-11-25 20:13:03 -0500

    Implemented UISearchController for starships only

--- ios-search/ViewController.swift
+++ ios-search/ViewController.swift
@@ -3,6 +3,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchContainerView: UIView!
+    var searchController: UISearchController!
+    
     var starshipsResponse: StarshipsResponse?
     var peopleResponse: PeopleResponse?
     var vehiclesResponse: VehiclesResponse?
@@ -11,19 +15,49 @@ class ViewController: UIViewController {
     var peopleNames: [String] = []
     var vehicleNames: [String] = []
     
+    var currentSearchResults: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.loadSwapiData()
         
-    }
-    
-    @IBAction func logValues() {
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        print("test starships---",self.starshipNames.debugDescription)
-        print("test vehicles----",self.vehicleNames.debugDescription)
-        print("test people -----", self.peopleNames.debugDescription)
+        starshipNames = currentSearchResults
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        // searchController.obscuresBackgroundDuringPresentation = false
+        searchContainerView.addSubview(searchController.searchBar)
+        searchController.searchBar.delegate = self
+        
+        
     }
     
+    func filterCurrentSearchResults(searchTerm: String){
+        
+        if searchTerm.count > 0 {
+            currentSearchResults = starshipNames
+            
+            let filteredResults = currentSearchResults.filter {$0.replacingOccurrences(of: " ", with: "").lowercased().contains(searchTerm.replacingOccurrences(of: " ", with: "").lowercased())}
+            
+            currentSearchResults  = filteredResults
+            tableView.reloadData()
+        }
+        
+    }
+    
+    func restoreDataSource() {
+        currentSearchResults = starshipNames
+        tableView.reloadData()
+    }
+    
+    
+    
+    @IBAction func refreshData(_ sender: Any) {
+        restoreDataSource()
+    }
     func loadSwapiData() {
         
         SwapiApiService.getStarshipResults(parameters: SwapiConstants.paramStarships, completion:{ response in
@@ -39,7 +73,7 @@ class ViewController: UIViewController {
         SwapiApiService.getVehicleResults(parameters: SwapiConstants.paramVehicles, completion: { response in
             
             self.vehiclesResponse = response
- 
+            
             for i in 0...9 {
                 
                 self.vehicleNames.append((self.vehiclesResponse?.results?[i].name!)!)
@@ -55,4 +89,59 @@ class ViewController: UIViewController {
             }
         })
     }
+}
+
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            filterCurrentSearchResults(searchTerm: searchText)
+        }
+    }
+    
+    
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchController.isActive = false
+        if let searchText = searchBar.text {
+            filterCurrentSearchResults(searchTerm: searchText)
+        }
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchController.isActive = false
+        
+        if let searchText = searchBar.text, !searchText.isEmpty {
+            restoreDataSource()
+        }
+    }
+    
+    
+    
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alertController = UIAlertController(title: "Selection", message: "Selected \(currentSearchResults[indexPath.row])", preferredStyle: .alert)
+        
+        searchController.isActive = false
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currentSearchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = currentSearchResults[indexPath.row]
+        return cell
+    }
+    
 }
